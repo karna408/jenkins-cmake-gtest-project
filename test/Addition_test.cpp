6e60ef156a56e3d0abdf6bc636d73af53018f951
karna408 <45122110+karna408@users.noreply.github.com>
2021-02-10 08:59:22 +0100

    Update Addition_test.cpp

--- test/Addition_test.cpp
+++ test/Addition_test.cpp
@@ -15,13 +15,13 @@ TEST(TestingAddition, CTOR) {
 TEST(TestingAddition, add) {
     zf::sample::Addition myAddition = zf::sample::Addition();
 		ASSERT_EQ(myAddition.add(1,1), 2);
-		ASSERT_NE(myAddition.add(10,10), 30);
-		ASSERT_GE(myAddition.add(10,10), 1);
+		ASSERT_NE(myAddition.add(10,10), 0);
+		ASSERT_GE(myAddition.add(10,10), 30);
 }
 
 TEST(TestingAddition, addID) {
     zf::sample::Addition myAddition = zf::sample::Addition();
-		ASSERT_EQ(myAddition.addID(1), 1);
+		ASSERT_EQ(myAddition.addID(1), 43);
 }
 
 TEST(TestingAddition, addOne) {
@@ -30,6 +30,6 @@ TEST(TestingAddition, addOne) {
 }
 
 TEST(TestingAddition, failure) {
-	  ASSERT_TRUE(true);
+	  ASSERT_TRUE(false);
 }
 
