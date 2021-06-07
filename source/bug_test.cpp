ce93af28b003f9a3151c379e44a1c503844ef79b
karna408 <45122110+karna408@users.noreply.github.com>
2021-06-07 23:45:59 +0200

    Update bug_test.cpp

--- source/bug_test.cpp
+++ source/bug_test.cpp
@@ -1,18 +1,1 @@
-#include <variant>
-#include <iostream>
-#include <string>
 
-using namespace std;
-using IntOrString = variant<int, string>;
-
-void stringize(IntOrString &ios) {
-    if(auto i = get_if<int>(&ios)) {
-        ios = to_string(*i);
-    }
-}
-int main() {
-    IntOrString ios = 12;
-    auto copy = ios;
-    stringize(ios);
-    cout << std::get<string>(ios) << '\n';
-}
